21d52dfb45b5d353c5fd03a81c76c557abee8139
BoGomolTrue <dokersmerk@mail.ru>
2019-12-11 09:36:33 +0700

    текст

---
+++ Змейка/Змейка/Blok.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Змейка
+{
+    class Blok:GameObject
+    {
+        public Blok()
+        {
+            my_Color = Color.Purple;
+        }
+        public void Add(Point a)
+        {
+            body.Add(a);
+        }
+    }
+}
---
+++ Змейка/Змейка/Food.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Змейка
+{
+    class Food:GameObject
+    {
+        public Food()
+        {
+            my_Color = Color.Red;
+        }
+
+        public void Add(Point a)
+        {
+            body.Add(a);
+        }
+        public Point RandomFood(int sizeX, int sizeY)
+        {
+            Random r = new Random();
+            Point a = new Point(r.Next(0, sizeX), r.Next(0, sizeY));
+            
+            /*for (int i = 0; i < body.Count; i++)
+            {
+                if (a.x == body[i].x && a.y == body[i].y)
+                {
+                    return RandomFood(sizeX, sizeY);
+                }
+            }*/
+            return a;
+        }
+    }
+}
